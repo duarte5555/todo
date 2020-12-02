13f5d164b43de93b6e43bfcc961be96d9bb8ede6
duarte5555 <71768918+duarte5555@users.noreply.github.com>
2020-12-02 13:29:38 +0000

    aula 2/12/2020

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from.import views
+
+urlpatterns = [
+    path('helloworld/', views.helloworld),
+    path('', views.tasklist, name='task-list'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+]
+    
--- todo/urls.py
+++ todo/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('helloworld/', helloworld),
+    path('admin/', admin.site.urls),
     path('', include('tasks.urls'))
 ]
